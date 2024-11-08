92700c76c235adeb5e4391481bc6582b2c4d030a
Davagulez <lezcadav@gmail.com>
2024-11-08 14:48:49 -0300

    Add(users): create entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import { IsBoolean, IsString } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true
+    })
+    email: string;
+
+    @Column('text')
+    password: string;
+
+    @Column('text')
+    fullName: string;
+
+    @Column('bool', {
+        unique: true
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    roles: string[];
+
+
+}
